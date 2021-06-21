a149b88018f78992ba7fef8a645c05713c259b3d
LoganYeu <73202655+LoganYeu@users.noreply.github.com>
2021-06-21 02:13:14 -0400

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,14 @@
+public class Task {
+    String objective;
+    int numberOfBreaks;
+
+    public Task(String objective, int numberOfBreaks) {
+        this.objective = objective;
+        this.numberOfBreaks = numberOfBreaks;
+    }
+
+
+
+
+}
+//thinking having a list of strings that prints information through a for loop at end for users plans.
---
+++ Time.java
@@ -0,0 +1,16 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Time {
+
+    public Time() {
+
+    }
+    /*
+    public static String timeGenerator(Scanner userInput) {
+        System.out.println("");
+    }
+
+     */
+}
