04ac35228c66260a1732559579c5ff01984d07c1
LoganYeu <73202655+LoganYeu@users.noreply.github.com>
2021-06-25 12:01:49 -0400

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,17 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Time {
+
+    public Time() {
+
+    }
+
+    public static LocalTime getLocalTime() {
+        return LocalTime.now();
+    }
+
+
+}
